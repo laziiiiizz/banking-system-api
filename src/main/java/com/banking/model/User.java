5fb77a5f9d4f550255c8e5bc0aadd8bf8dfa0f99
Abdulaziz Abduvaliev <aabduva@gmail.com>
2026-05-24 16:05:33 -0400

    Adding users entity to model layer

---
+++ src/main/java/com/banking/model/User.java
@@ -0,0 +1,33 @@
+package com.banking.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "users")
+@Data@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+
+}
